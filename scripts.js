356a21882e2fc46f0d2428248c025aa00d0a63e7
Hans Danielsson <Hans.Ro.Danielsson@gmail.com>
2024-10-23 18:52:17 +0200

    Save spaces

--- scripts.js
+++ scripts.js
@@ -2,11 +2,11 @@
 const Animal = require('./animal.js')
 
 // Skapa ett objekt av Animal
-let myAnimal = new Animal("Elvis", 15, "Cat")
+const myAnimal = new Animal('Elvis', 15, 'Cat')
 
 console.log(myAnimal.name)
 console.log(myAnimal.toString())
 
-//Birthday
+// Birthday
 myAnimal.aging()
 console.log(myAnimal.toString())
--- sum.js
+++ sum.js
@@ -1,11 +1,11 @@
-function sum(a, b) {
-    return a + b
+function sum (a, b) {
+  return a + b
 }
 
-function sub(a, b) {
-    return a - b
+function sub (a, b) {
+  return a - b
 }
 
-//export function
+// export function
 
 module.exports = {sum, sub}
